532a3f57ff7257a11befb56ed39fd66b403bba3d
tifoaudy <165150200111168@mail.ub.ac.id>
2019-01-22 18:46:11 +0700

    initial commit

---
+++ SmackChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmackChat
+//
+//  Created by Tifo Audi Alif Putra on 22/01/19.
+//  Copyright © 2019 BCC FILKOM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
